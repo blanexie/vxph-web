579f609ffade6244cccde5a13ba82a48aa91c364
xiezhicheng <xiezhicheng@xiaomi.com>
2023-11-02 19:23:49 +0800

    init

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,22 @@
 <template>
-  <el-form ref="formRef" :model="userInfo" label-width="4rem" class="demo-dynamic">
-    <el-form-item prop="text" label="用户名" :rules="nickNameRule">
-      <el-input v-model="userInfo.nickName" />
-    </el-form-item>
-    <el-form-item prop="password" label="密码" :rules="passwordRule">
-      <el-input v-model="userInfo.password"  type = "password" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm(userInfo)">登录</el-button>
-      <el-button @click="toSignUp()">注册</el-button>
-    </el-form-item>
-  </el-form>
+  <el-card class="box-card" shadow="always">
+    <el-form ref="formRef" :model="userInfo" label-width="4rem" >
+      <el-form-item prop="text" label="用户名" :rules="nickNameRule">
+        <el-input v-model="userInfo.nickName" />
+      </el-form-item>
+      <el-form-item prop="password" label="密码" :rules="passwordRule">
+        <el-input v-model="userInfo.password" type="password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(userInfo)">登录</el-button>
+        <el-button @click="toSignUp()">注册</el-button>
+      </el-form-item>
+    </el-form></el-card>
 </template>
 <style scoped>
-.demo-dynamic {
-  width: 25rem;
-  background-color: rgb(250, 250, 250);
+.box-card {
   padding: 2rem;
-  border: 1px black solid;
-  border-radius: 1rem;
+  width: 480px;
 }
 </style>
 <script lang="ts" setup>
@@ -53,6 +51,10 @@ const userInfo = reactive<{
 
 const submitForm = (userInfo) => {
   loginReq.login(userInfo.nickName, userInfo.password).then(resp => {
+    const data = resp.data
+    if (data.code != 200) {
+
+    }
     console.log(resp)
   })
 }
