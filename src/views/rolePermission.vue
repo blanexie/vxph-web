49f529265d49908f2e2c94c7726f3025364ddae5
xiezhicheng <xiezhicheng@xiaomi.com>
2023-12-14 19:07:49 +0800

    rp

---
+++ src/views/rolePermission.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card-div">
+    <el-descriptions
+        class="margin-top"
+        title="Without border"
+        :column="3"
+    >
+      <el-descriptions-item label="code">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="name">18100000000</el-descriptions-item>
+      <el-descriptions-item label="description">Suzhou</el-descriptions-item>
+      <el-descriptions-item label="createTime">Suzhou</el-descriptions-item>
+      <el-descriptions-item label="updateTime">Suzhou</el-descriptions-item>
+      <el-descriptions-item label="status">
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="Address"
+      >No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
+
+  <div class="card-div">
+
+  </div>
+</template>
+<script setup lang="ts">
+import {reactive, computed, ref, onMounted} from 'vue'
+import {Role, Permission} from '@/common/class';
+
+
+const role = reactive<{
+  role:Role,
+  allowPermissions:Permission[],
+
+
+}>(null)
+
+
+onMounted(() => {
+
+
+})
+
+
+</script>
