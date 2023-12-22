973a732c0a5faa659aad081da6c78b40d9066e29
xiezhicheng <xiezhicheng@xiaomi.com>
2023-12-22 17:03:34 +0800

    role

--- src/views/role.vue
+++ src/views/role.vue
@@ -9,13 +9,13 @@
   <div class="card-div">
     <br>
     <el-table :data="tableData" :stripe="true" border :highlight-current-row="true" style="width: 100%"
-              table-layout="auto">
-      <el-table-column fixed prop="name" label="name"/>
-      <el-table-column prop="code" label="code"/>
-      <el-table-column prop="description" label="description"/>
-      <el-table-column prop="status" label="status"/>
-      <el-table-column prop="createTime" label="createTime"/>
-      <el-table-column prop="updateTime" label="updateTime"/>
+      table-layout="auto">
+      <el-table-column fixed prop="name" label="name" />
+      <el-table-column prop="code" label="code" />
+      <el-table-column prop="description" label="description" />
+      <el-table-column prop="status" label="status" />
+      <el-table-column prop="createTime" label="createTime" />
+      <el-table-column prop="updateTime" label="updateTime" />
       <el-table-column label="operation">
         <template #default="scope">
           <el-button link type="primary" size="small" @click.prevent="deleteRole(scope.row.code)">
@@ -35,29 +35,28 @@
       <el-form label-position="right" label-width="100px" :model="drawerData.rowData">
         <el-form-item label="修改">
           <el-switch v-model="drawerData.editDisable" :inactive-value="true" :active-value="false" class="mb-2"
-                     active-text=""/>
+            active-text="" />
         </el-form-item>
         <el-form-item label="code">
-          <el-input v-model="drawerData.rowData.code" :disabled="drawerData.codeEditDisable"/>
+          <el-input v-model="drawerData.rowData.code" :disabled="drawerData.codeEditDisable" />
         </el-form-item>
         <el-form-item label="name">
-          <el-input v-model="drawerData.rowData.name" :disabled="drawerData.editDisable"/>
+          <el-input v-model="drawerData.rowData.name" :disabled="drawerData.editDisable" />
         </el-form-item>
         <el-form-item label="description">
-          <el-input type="textarea" v-model="drawerData.rowData.description" :disabled="drawerData.editDisable"/>
+          <el-input type="textarea" v-model="drawerData.rowData.description" :disabled="drawerData.editDisable" />
         </el-form-item>
         <el-form-item label="status">
-          <el-input v-model="drawerData.rowData.status" :disabled="drawerData.editDisable"/>
+          <el-input v-model="drawerData.rowData.status" :disabled="drawerData.editDisable" />
         </el-form-item>
         <el-form-item label="createTime">
-          <el-input v-model="drawerData.rowData.createTime" disabled/>
+          <el-input v-model="drawerData.rowData.createTime" disabled />
         </el-form-item>
         <el-form-item label="updateTime">
-          <el-input v-model="drawerData.rowData.updateTime" disabled/>
+          <el-input v-model="drawerData.rowData.updateTime" disabled />
         </el-form-item>
         <el-form-item>
-          <el-button :disabled="drawerData.editDisable" @click="addRoleDrawer(drawerData.rowData)"
-                     type="primary">提交
+          <el-button :disabled="drawerData.editDisable" @click="addRoleDrawer(drawerData.rowData)" type="primary">提交
           </el-button>
         </el-form-item>
       </el-form>
@@ -65,42 +64,40 @@
 
     <el-drawer v-model="drawerPermission.show" title="授权" size="700" direction="rtl">
       <div> 角色 : {{ drawerPermission.role.name }}</div>
-      <br/>
+      <br />
       <div>
-        <el-select class="w-50" v-model="drawerPermission.copyRole" filterable reserve-keyword
-                   placeholder="请选择要复制的角色" :loading="drawerPermission.selectloading">
-          <el-option v-for="item in tableData" :key="item.code" :label="item.name" :value="item.code"/>
+        <el-select class="w-50" v-model="drawerPermission.copyRole" filterable reserve-keyword placeholder="请选择要复制的角色"
+          :loading="drawerPermission.selectloading">
+          <el-option v-for="item in tableData" :key="item.code" :label="item.name" :value="item.code" />
         </el-select>
-        <el-button type="primary"
-                   @click="copyRolePermission(drawerPermission.copyRole, drawerPermission.role)">复制角色权限
+        <el-button type="primary" @click="copyRolePermission(drawerPermission.copyRole, drawerPermission.role)">复制角色权限
         </el-button>
       </div>
-      <br/>
+      <br />
       <div>
         <el-select class="w-50" v-model="drawerPermission.selectPermissionCodes" multiple filterable remote
-                   reserve-keyword placeholder="请输入权限关键字" remote-show-suffix
-                   :remote-method="searchRemotePermission"
-                   :loading="drawerPermission.selectloading">
+          reserve-keyword placeholder="请输入权限关键字" remote-show-suffix :remote-method="searchRemotePermission"
+          :loading="drawerPermission.selectloading">
           <el-option v-for="item in drawerPermission.searchPermissions" :key="item.code" :label="item.name"
-                     :value="item.code"/>
+            :value="item.code" />
         </el-select>
 
         <el-button type="primary"
-                   @click="addRolePermission(drawerPermission.role, drawerPermission.selectPermissionCodes)">添加权限
+          @click="addRolePermission(drawerPermission.role, drawerPermission.selectPermissionCodes)">添加权限
         </el-button>
       </div>
       <br>
       <div>包含的权限：</div>
       <br>
       <el-table :data="drawerPermission.role.permissions" border :stripe="true" :highlight-current-row="true"
-                style="width: 100%" table-layout="auto">
-        <el-table-column fixed prop="name" label="name"/>
-        <el-table-column prop="code" label="code"/>
-        <el-table-column prop="type" label="type"/>
+        style="width: 100%" table-layout="auto">
+        <el-table-column fixed prop="name" label="name" />
+        <el-table-column prop="code" label="code" />
+        <el-table-column prop="type" label="type" />
         <el-table-column fixed="right" label="Operations" width="120">
           <template #default="scope">
             <el-button link type="primary" size="small"
-                       @click.prevent="removeRolePermission(drawerPermission.role, scope.row)">
+              @click.prevent="removeRolePermission(drawerPermission.role, scope.row)">
               Remove
             </el-button>
           </template>
@@ -115,10 +112,11 @@
 }
 </style>
 <script lang="ts" setup>
-import {reactive, computed, ref, onMounted} from 'vue'
-import {roleReq, permissionReq} from "../common/request"
-import {duplicate} from '../common/util';
-import {Role, Permission} from '../common/class';
+import { reactive, computed, ref, onMounted } from 'vue'
+import { roleReq, permissionReq } from "../common/request"
+import { duplicate } from '../common/util';
+import { Role, Permission } from '../common/class';
+import router from '@/common/route';
 
 
 const drawerPermission = ref<{
@@ -258,18 +256,16 @@ const searchRemotePermission = (query: string) => {
     return
   }
   const dp = drawerPermission.value
-  permissionReq.list({page: 1, pageSize: 10, searchKey: query})
-      .then(resp => {
-        dp.searchPermissions = resp.data.content
-        console.log("searchRemotePermission", dp.searchPermissions)
-        dp.selectloading = false
-      })
+  permissionReq.list({ page: 1, pageSize: 10, searchKey: query })
+    .then(resp => {
+      dp.searchPermissions = resp.data.content
+      console.log("searchRemotePermission", dp.searchPermissions)
+      dp.selectloading = false
+    })
 }
 
 const updatePermission = (row: any) => {
-  drawerPermission.value.role = row
-  drawerPermission.value.show = true
-  drawerPermission.value.editDisable = true
+  router.push("/rolePermission?roleCode=" + row.code)
 }
 
 onMounted(() => {
--- src/views/rolePermission.vue
+++ src/views/rolePermission.vue
@@ -14,19 +14,42 @@
   </div>
 
   <div class="card-div">
-    <h3>权限列表</h3> <br />
-    <el-table :data="rolePermission.page" border style="width: 100%">
-      <el-table-column prop="code" label="Code" width="180" />
-      <el-table-column prop="name" label="Name" width="180" />
-      <el-table-column prop="type" label="type" />
-      <el-table-column prop="description" label="Description" />
-      <el-table-column prop="status" label="Status" />
-      <el-table-column prop="createTime" label="CreateTime" />
-      <el-table-column prop="updateTime" label="UpdateTime" />
-    </el-table>
+    <h3>权限列表</h3>
     <br />
-    <el-pagination background layout="prev, pager, next" @current-change="pageChange"
-      :total="rolePermission.permissions.length" />
+
+    <el-radio-group @change="pageChange(1)" v-model="rolePermission.filterType" label="label position">
+      <el-radio-button label="all">全部</el-radio-button>
+      <el-radio-button label="allow">有权限</el-radio-button>
+      <el-radio-button label="notAllow">无权限</el-radio-button>
+    </el-radio-group>
+ 
+    <el-table :data="rolePermission.page" border style="width: 100%">
+      <el-table-column prop="code" label="Code" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="type" label="Type" />
+
+      <el-table-column label="Status">
+        <template #default="scope">
+          <el-tag v-if="scope.row.has" class="ml-2" type="success"> 已授权 </el-tag>
+          <el-tag v-if="!scope.row.has" class="ml-2" type="warning"> 未授权 </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column fixed="right" label="Operations">
+        <template #default="scope">
+          <el-button v-if="!scope.row.has" link type="primary" size="small" @click.prevent="allow(scope.row)">
+            授权
+          </el-button>
+          <el-button v-if="scope.row.has" link type="primary" size="small" @click.prevent="notAllow(scope.row)">
+            取消授权
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <br />
+    <el-pagination background layout="prev, pager, next" @current-change="pageChange" :total="rolePermission.count" />
+
   </div>
 </template>
 <script setup lang="ts">
@@ -34,49 +57,118 @@ import { reactive, computed, ref, onMounted } from 'vue'
 import { Role, Permission } from '@/common/class';
 import { roleReq, permissionReq } from "@/common/request";
 import { useRoute, useRouter } from 'vue-router'
+import { Search } from '@element-plus/icons-vue'
 
+class PermissionExtend extends Permission {
+  has: boolean | undefined
+}
 const route = useRoute()
-
 const rolePermission = ref<{
+  searchWord: string,
   role: Role,
+  count: number,
   pageNo: number,
-  page: Permission[],
-  permissions: Permission[],
-  allowPermission: Permission[],
+  filterType: string,
+  page: PermissionExtend[],
+  permissions: PermissionExtend[],
+  allowPermission: PermissionExtend[],
 }>({
+  searchWord: "",
+  count: 10,
   pageNo: 1,
-  page: [],
   role: new Role(),
+  filterType: "all",
+  page: [],
   permissions: [],
   allowPermission: []
 })
 
-const pageChange = (pageNo: number) => {
-  let start = (pageNo - 1) * 10
-  let end = pageNo * 10
+const notAllow = (pe: PermissionExtend) => {
   let rp = rolePermission.value
-  rp.page = rp.permissions.slice(start, end)
+  rp.allowPermission = rp.allowPermission.filter(it => it.code !== pe.code)
+  rp.permissions = rp.permissions.map(it => {
+    if (it.code == pe.code) {
+      it.has = false
+    }
+    return it
+  })
+  rp.role.permissions = rp.allowPermission
+
+  roleReq.save(rp.role).then(resp => {
+    rp.role = resp.data
+  })
 }
 
+const allow = (pe: PermissionExtend) => {
+  let rp = rolePermission.value
+  rp.allowPermission.push(pe)
+  pe.has = true
+  rp.role.permissions = rp.allowPermission
+
+  roleReq.save(rp.role).then(resp => {
+    rp.role = resp.data
+  })
+}
+
+const pageChange = (pageNo: number) => {
+  let rp = rolePermission.value
+  let codes = rp.allowPermission.map(it => it.code!)
+  let filterType = rp.filterType
+  let start = (pageNo - 1) * 10
+  let end = (pageNo) * 10
+  if (filterType == "all") {
+    rp.count = rp.permissions.length
+    rp.page = rp.permissions.slice(start, end)
+  } else if (filterType == "allow") {
+    rp.count = rp.allowPermission.length
+    rp.page = rp.allowPermission.slice(start, end)
+  } else if (filterType == "notAllow") {
+    let fp = rp.permissions.filter(it => !codes.includes(it.code!))
+    rp.count = fp.length
+    rp.page = fp.slice(start, end)
+  }
+}
 
 onMounted(() => {
   let rp = rolePermission.value
   let roleCode = route.query.roleCode
-
   roleReq.findByCode(roleCode).then(resp => {
     rp.role = resp.data as Role
   })
-
   roleReq.findPermissionByCode(roleCode).then(resp => {
-    rp.allowPermission = resp.data as Permission[]
+    let pi = resp.data as Permission[]
+    rp.allowPermission = pi.map(it => {
+      let pe = convert(it)
+      pe.has = true
+      return pe
+    })
+    let codes = rp.allowPermission.map(it => it.code!)
+    permissionReq.findAll().then(resp => {
+      let pi = resp.data as Permission[]
+      rp.permissions = pi.map(it => convertExentd(codes, it))
+      rp.page = rp.permissions.slice(0, 10)
+      rp.count = rp.permissions.length
+    })
   })
-
-  permissionReq.findAll().then(resp => {
-    rp.permissions = resp.data
-    rp.page = rp.permissions.slice(0, 10)
-  })
-
 })
 
+const convert = (it: Permission): PermissionExtend => {
+  let pe = new PermissionExtend()
+  pe.code = it.code
+  pe.createTime = it.createTime
+  pe.description = it.description
+  pe.name = it.name
+  pe.status = it.status
+  pe.type = it.type
+  pe.updateTime = it.updateTime
+  pe.versionNo = it.versionNo
+  pe.has = false
+  return pe
+}
 
+const convertExentd = (codes: string[], it: Permission): PermissionExtend => {
+  let pe = convert(it)
+  pe.has = codes.includes(it.code!!)
+  return pe
+}
 </script>
